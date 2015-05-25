573c56b35932dd86a25b448b02f656d62f45f05d
yugklim <yugklim@gmail.com>
2015-05-25 18:45:13 +0300

    initial

---
+++ IpCompare/IpCompareUtility.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace IpCompare
+{
+    public static class IpCompareUtility
+    {
+        // regex to split ip range - f.e., splits 11.22.33.44/55 into two groups - 11.22.33.44 and 55
+        private static string regExToSplitRange = @"^\b((?:\d{1,3}\.){3}(?:\d{1,3}){1})(?:(?:\\|\/)(\d{1,3})){0,1}\b";
+        // regex to split the ip into parts - f.e., splits 11.22.33.44 into 4 parts: 11, 22, 33, and 44
+        // it doesn't split wrong ip? - 11.22.33 is split into 0 groups? - so you can use it to verify the correctness of ip
+        private static string regExToSplitIp = @"^\b(\d{1,3})(?:\.{1})(\d{1,3})(?:\.{1})(\d{1,3})(?:\.{1})(\d{1,3})\b";
+
+        public static IEnumerable<string> GetIpParts(this string ip)
+        {
+            List<string> ipParts = Regex.Split(ip, regExToSplitIp).ToList();
+            ipParts.RemoveAll(string.IsNullOrEmpty);
+            return ipParts;
+        }
+
+        public static IEnumerable<string> GetIpRangeParts(this string ipRange)
+        {
+            List<string> rangeParts = Regex.Split(ipRange, regExToSplitRange).ToList();
+            rangeParts.RemoveAll(string.IsNullOrEmpty);
+            return rangeParts;
+        }
+
+        public static bool IsAValidIp(this string value)
+        {
+            List<string> ipParts = GetIpParts(value).ToList();
+            int ip;
+            return ipParts.Count() == 4
+                && ipParts.All(ipPart => int.TryParse(ipPart, out ip) && ip <= 255)
+                && (ipParts.Sum(ipPart => int.Parse(ipPart)) > 0);
+        }
+
+        public static bool IsAValidIpRange(this string value)
+        {
+            List<string> ipRangeParts = GetIpRangeParts(value).ToList();
+            int rangeUpperLimit;
+            return ipRangeParts.Count() == 2 && IsAValidIp(ipRangeParts[0])
+                && int.TryParse(ipRangeParts[1], out rangeUpperLimit) && rangeUpperLimit > 0 && rangeUpperLimit <= 255
+                && int.Parse(GetIpParts(ipRangeParts[0]).ToList()[3]) <= rangeUpperLimit;
+        }
+
+        public static bool IsIpRangeEqual(this string range1, string range2)
+        {
+            if (IsAValidIpRange(range1) && IsAValidIpRange(range2))
+            {
+                List<string> range1Parts = GetIpRangeParts(range1).ToList();
+                List<string> range2Parts = GetIpRangeParts(range2).ToList();
+                return range1Parts[0].IsIpEqualTo(range2Parts[0])
+                    && int.Parse(range1Parts[1]) == int.Parse(range2Parts[1]);
+            }
+
+            return false;
+        }
+
+        public static bool IsIpEqualTo(this string ip1, string ip2)
+        {
+            if (IsAValidIp(ip1) && IsAValidIp(ip2))
+            {
+                List<string> ip1Parts = GetIpParts(ip1).ToList();
+                List<string> ip2Parts = GetIpParts(ip2).ToList();
+                return int.Parse(ip1Parts[0]) == int.Parse(ip2Parts[0])
+                       && int.Parse(ip1Parts[1]) == int.Parse(ip2Parts[1])
+                       && int.Parse(ip1Parts[2]) == int.Parse(ip2Parts[2])
+                       && int.Parse(ip1Parts[3]) == int.Parse(ip2Parts[3]);
+            }
+
+            return false;
+        }
+
+        public static bool IsIpGreaterOrEqualThan(this string ip1, string ip2)
+        {
+            if (IsAValidIp(ip1) && IsAValidIp(ip2))
+            {
+                List<string> ip1Parts = GetIpParts(ip1).ToList();
+                List<string> ip2Parts = GetIpParts(ip2).ToList();
+                return int.Parse(ip1Parts[0]) == int.Parse(ip2Parts[0])
+                       && int.Parse(ip1Parts[1]) == int.Parse(ip2Parts[1])
+                       && int.Parse(ip1Parts[2]) == int.Parse(ip2Parts[2])
+                       && int.Parse(ip1Parts[3]) >= int.Parse(ip2Parts[3]);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// verifies whether addressVerified is in range. range can be a single ip: 11.11.11.11
+        /// </summary>
+        /// <param name="rangeOrIp"></param>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsIpInRange(this string ip, string rangeOrIp)
+        {
+            if (!ip.IsAValidIp() || !(rangeOrIp.IsAValidIpRange() || rangeOrIp.IsAValidIp()))
+            {
+                return false;
+            }
+
+            // just one address
+            List<string> rangeParts = rangeOrIp.GetIpRangeParts().ToList();
+            string startIp = rangeParts[0];
+            if (rangeOrIp.IsAValidIp())
+            {
+                return startIp.IsIpEqualTo(ip);
+            }
+
+            // deal with range really
+            List<string> ipParts = ip.GetIpParts().ToList();
+            string endIp = rangeParts[1];
+            return ip.IsIpGreaterOrEqualThan(startIp) && int.Parse(endIp) >= int.Parse(ipParts[3]);
+        }
+    }
+}
---
+++ IpCompareTests/IpCompareTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IpCompare;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IpCompareTests
+{
+    [TestClass]
+    public class IpCompareTests
+    {
+        [TestMethod]
+        public void Verify_Whether_GetIpParts_Returns_Correct_Results()
+        {
+            string ip = "11.22.333.4";
+            List<string> ipParts = ip.GetIpParts().ToList();
+            Assert.AreEqual(ipParts.Count(), 4);
+            Assert.AreEqual(ipParts[0], "11");
+            Assert.AreEqual(ipParts[1], "22");
+            Assert.AreEqual(ipParts[2], "333");
+            Assert.AreEqual(ipParts[3], "4");
+
+            ip = "11.22.333";
+            ipParts = ip.GetIpParts().ToList();
+            Assert.AreNotEqual(ipParts.Count(), 4);
+        }
+
+        [TestMethod]
+        public void Verify_Whether_IsStringAValidIp_Returns_Correct_Results()
+        {
+            Assert.IsTrue("11.11.11.11".IsAValidIp());
+            Assert.IsFalse(@"11.11.11.11\15".IsAValidIp());
+            Assert.IsFalse(@"11.11.11".IsAValidIp());
+            Assert.IsFalse("11.11.11.1d".IsAValidIp());
+            Assert.IsTrue("11.11.255.12".IsAValidIp());
+            Assert.IsFalse("11.11.256.12".IsAValidIp());
+            Assert.IsFalse("0.0.0.0".IsAValidIp());
+            Assert.IsFalse("000.000.000.000".IsAValidIp());
+        }
+
+        [TestMethod]
+        public void Verify_Whether_GetIpRangeParts_Returns_Correct_Results()
+        {
+            string ipRange = "11.11.11.11/15";
+            List<string> ipRangeParts = ipRange.GetIpRangeParts().ToList();
+            Assert.AreEqual(ipRangeParts.Count(), 2);
+            Assert.AreEqual(ipRangeParts[0], "11.11.11.11");
+            Assert.AreEqual(ipRangeParts[1], "15");
+
+            ipRange = @"11.11.11.11\15";
+            ipRangeParts = ipRange.GetIpRangeParts().ToList();
+            Assert.AreEqual(ipRangeParts.Count(), 2);
+            Assert.AreEqual(ipRangeParts[0], "11.11.11.11");
+            Assert.AreEqual(ipRangeParts[1], "15");
+        }
+
+        [TestMethod]
+        public void Verify_Whether_IsStringAValidIpRange_Returns_Correct_Results()
+        {
+            Assert.IsTrue("11.11.11.11/15".IsAValidIpRange());
+            Assert.IsTrue(@"11.11.11.11\15".IsAValidIpRange());
+            Assert.IsFalse(@"11.11.11.11\9".IsAValidIpRange());
+            Assert.IsFalse("11.11.11.11".IsAValidIpRange());
+            Assert.IsFalse("11.11.11.1d/113".IsAValidIpRange());
+            Assert.IsFalse(@"11.11.11.11\ss".IsAValidIpRange());
+            Assert.IsFalse(@"11.11.11.11\0".IsAValidIpRange());
+            Assert.IsTrue(@"11.11.11.11\255".IsAValidIpRange());
+            Assert.IsFalse(@"11.11.11.11\256".IsAValidIpRange());
+        }
+
+        [TestMethod]
+        public void Verify_Whether_IpLessOrEqualThan_Works_Right()
+        {
+            Assert.IsTrue("11.11.11.11".IsIpEqualTo("11.11.11.11"));
+            Assert.IsFalse("11.11.11.11".IsIpEqualTo("11.11.14.12"));
+            Assert.IsFalse("11.11.11.112".IsIpEqualTo("11.11.11.1"));
+            Assert.IsFalse("11.11.bb.11".IsIpEqualTo("11.11.bb.12"));
+        }
+
+        [TestMethod]
+        public void Verify_Whether_IpRangeEqual_Works_Right()
+        {
+            Assert.IsFalse("11.11.11.11/11".IsIpRangeEqual("11.11.11.11/12"));
+            Assert.IsFalse("11.11.11.11/11".IsIpRangeEqual(@"11.11.11.11\12"));
+            Assert.IsFalse("11.11.11.11/13".IsIpRangeEqual("11.11.11.11/12"));
+            Assert.IsFalse("11.11.11.xx/11".IsIpRangeEqual("11.11.11.xx/12"));
+            Assert.IsFalse("11.11.11.11/11".IsIpRangeEqual("11.11.11.11/12"));
+        }
+
+        [TestMethod]
+        public void Verify_IsIpInRange_Works_Right()
+        {
+            Assert.IsTrue("11.11.11.11".IsIpInRange("11.11.11.11"));
+            Assert.IsTrue("11.11.11.12".IsIpInRange("11.11.11.11/15"));
+            Assert.IsTrue("11.11.11.12".IsIpInRange(@"11.11.11.11\15"));
+            Assert.IsFalse("11.11.11.11".IsIpInRange("11.11.11.12"));
+        }
+
+    }
+}
